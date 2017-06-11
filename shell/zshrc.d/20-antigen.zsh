7a831cff45fd13d642f8ac57010af1c06e6522c6
Pascal Mathis <git@development.smx.li>
2017-06-11 17:03:39 +0200

    Changed polybar to contain main/side configs, adjusted bspwmrc for workstation (multi-system support is next), added virtualenvwrapper to zsh

--- shell/zshrc.d/20-antigen.zsh
+++ shell/zshrc.d/20-antigen.zsh
@@ -2,6 +2,7 @@ source "${DOTFILES}/thirdparty/antigen/antigen.zsh"
 antigen use oh-my-zsh
 antigen bundle dirhistory
 antigen bundle systemd
+antigen bundle virtualenvwrapper
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen theme norm
 antigen apply
