c4cbd5313d6585ec0843d98109dcd06bcf3e321e
Pascal Mathis <git@development.smx.li>
2017-07-10 19:40:01 +0200

    Replaced SauceCodePro with Meslo

--- shell/zshrc.d/20-zplug.zsh
+++ shell/zshrc.d/20-zplug.zsh
@@ -5,7 +5,6 @@ zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 zplug 'mafredri/zsh-async'
 zplug 'sindresorhus/pure', use:pure.zsh, as:theme
 
-zplug 'zsh-users/zsh-autosuggestions'
 zplug 'zsh-users/zsh-completions', defer:2
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'zsh-users/zsh-history-substring-search', defer:3
