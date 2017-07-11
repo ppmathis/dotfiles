c8695afc73ffd7a63e3a73ea75e73acb41aafa45
Pascal Mathis <git@development.smx.li>
2017-07-11 23:10:55 +0200

    Replaced ZSH theme with ppmathis/snappy

--- shell/zshrc.d/20-zplug.zsh
+++ shell/zshrc.d/20-zplug.zsh
@@ -3,7 +3,7 @@ source "${DOTFILES}/thirdparty/zplug/init.zsh"
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 zplug 'mafredri/zsh-async'
-zplug 'sindresorhus/pure', use:pure.zsh, as:theme
+zplug 'ppmathis/snappy', use:snappy.zsh, as:theme
 
 zplug 'zsh-users/zsh-completions', defer:2
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
