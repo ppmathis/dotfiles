315b527a052c4496a92cca1f75510c6bb7d64ccf
Pascal Mathis <git@development.smx.li>
2017-07-11 23:15:32 +0200

    Fixed auto-title script for ZSH

--- shell/zshrc.d/80-auto-title.zsh
+++ shell/zshrc.d/80-auto-title.zsh
@@ -1,4 +1,34 @@
-function omz_termsupport_precmd {
+function title {
+	emulate -L zsh
+	setopt prompt_subst
+
+	[[ "$EMACS" == *term* ]] && return
+	: ${2=$1}
+
+	case "$TERM" in
+		cygwin|xterm*|putty*|rxvt*|ansi)
+			print -Pn "\e]2;$2:q\a"
+			print -Pn "\e]1;$1:q\a"
+			;;
+		screen*)
+			print -Pn "\ek$1:q\e\\"
+			;;
+		*)
+			if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+				print -Pn "\e]2;$2:q\a"
+				print -Pn "\e]1;$1:q\a"
+			else
+				if [[ -n "$terminfo[fsl]" ]] && [[ -n "$terminfo[tsl]" ]]; then
+		echoti tsl
+		print -Pn "$1"
+		echoti fsl
+	fi
+			fi
+			;;
+	esac
+}
+
+function dotfiles_autotitle_precmd {
 	emulate -L zsh
 
 	if [[ "$DISABLE_AUTO_TITLE" == true ]]; then
@@ -8,7 +38,7 @@ function omz_termsupport_precmd {
 	title '${ZSH_THEME_TERM_TAB_TITLE_PREFIX}%~' '${ZSH_THEME_TERM_TITLE_PREFIX}%~'
 }
 
-function omz_termsupport_preexec {
+function dotfiles_autotitle_preexec {
 	emulate -L zsh
 	setopt extended_glob
 
@@ -28,3 +58,6 @@ else
 	ZSH_THEME_TERM_TITLE_PREFIX="%n@%M: "
 fi
 ZSH_THEME_TERM_TAB_TITLE_PREFIX=$ZSH_THEME_TERM_TITLE_IDLE
+
+precmd_functions+=(dotfiles_autotitle_precmd)
+preexec_functions+=(dotfiles_autotitle_preexec)
