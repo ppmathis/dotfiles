9de8e55d51e88693783afe660b2b513e1a4ad765
Pascal Mathis <pascal.mathis@git.smx.li>
2017-09-24 05:42:39 +0200

    Wallpaper script rewritten to use SSBL

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+#!/usr/bin/env bash
+source "${HOME}/.local/lib/ssbl.sh"
 
-SCREEN_RESOLUTION=$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
-WALLPAPER_BASE_FOLDER="${HOME}/pictures/wallpapers"
-WALLPAPER_RESOLUTION_FOLDER="${WALLPAPER_BASE_FOLDER}/${SCREEN_RESOLUTION}/"
+main() {
+	# Detect screen resolution
+	ssbl_log "Detecting screen resolution with Xrandr..."
+	local _screen_resolution="$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')"
+	ssbl_log "Detected screen resolution: ${_screen_resolution}"
 
-if [ -d "${WALLPAPER_RESOLUTION_FOLDER}" ]; then
-	feh --randomize --bg-fill --no-fehbg --no-xinerama "${WALLPAPER_RESOLUTION_FOLDER}"
-else
-	notify-send -u normal "Wallpaper Rotation" "Unable to fetch random wallpaper\!\nUnsupported resolution: ${SCREEN_RESOLUTION}"
-fi
+	# Build folder paths
+	local _wp_base_path="${HOME}/pictures/wallpapers"
+	local _wp_resolution_path="${_wp_base_path}/${_screen_resolution}/"
+	ssbl_log "Wallpaper Base Path: ${_wp_base_path}"
+	ssbl_log "Wallpaper Resolution Path: ${_wp_resolution_path}"
+
+	# Execute feh if wallpaper resolution folder exists
+	if [[ -d "${_wp_resolution_path}" ]]; then
+		ssbl_log_cmd feh --randomize --bg-fill --no-fehbg --no-xinerama "${_wp_resolution_path}"
+	else
+		ssbl_log_cmd notify-send -u normal "Wallpaper Rotation" "Unable to fetch random wallpaper\!\nUnsupported resolution: ${_screen_resolution}"
+	fi
+}
+
+main "${@}"
