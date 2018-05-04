16bdd6179f400b60e2105d402db02bf25ab58265
Pascal Mathis <mail@pascalmathis.com>
2018-05-04 19:16:36 +0200

    shell: Automatically set SSH_AUTH_SOCK

--- shell/bash.d/environment.sh
+++ shell/bash.d/environment.sh
@@ -7,3 +7,11 @@ export HISTCONTROL="ignoreboth"
 
 _path_=$(command -v "vim") && export EDITOR="${_path_}"
 _path_=$(command -v "less") && export MANPAGER="${_path_} -X"
+
+if [[ -z "${SSH_AUTH_SOCK}" ]] || ([[ -z "${SSH_CLIENT}" ]] & [[ -z "${SSH_TTY}" ]]); then
+	if [[ -S "${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh" ]]; then
+		export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+	else
+		eval "$(ssh-agent -s)"
+	fi
+fi
