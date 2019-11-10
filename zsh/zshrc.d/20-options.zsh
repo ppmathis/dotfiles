e2749a70d83fc0ccc382194b3e03adb39c2c1012
Pascal Mathis <mail@pascalmathis.com>
2019-11-10 13:29:19 +0100

    zsh: Add new shell aliases and options

---
+++ zsh/zshrc.d/20-options.zsh
@@ -0,0 +1,2 @@
+setopt globdots
+setopt rmstarsilent
--- zsh/zshrc.d/90-aliases.zsh
+++ zsh/zshrc.d/90-aliases.zsh
@@ -1,19 +1,41 @@
+# Resolve nested aliases
 alias sudo='sudo '
+alias watch='watch '
+alias xargs='xargs '
 
+# Directory traversal
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-alias rm='rm --preserve-root'
+# Safety measures
+alias rm='rm -I --preserve-root'
 alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
+alias mv='mv -i'
+alias cp='cp -i'
+
+# Terminal / Shell commands
+alias reload="exec ${SHELL} -l"
+alias bell='tput bel'
+alias path='echo -e "${PATH//:/\\n}"'
+
+# Command shortcuts
+alias dkr='sudo -- docker'
+alias log='sudo -- journalctl'
+alias sys='sudo -- systemctl'
+
+# Command tweaks
+alias wget='wget --content-disposition'
+alias su='su -l'
+
+# Useful commands
+alias ll='ls -lAhF --group-directories-first'
+alias l='ls -CF'
+alias tf='truncate -s0'
 
 alias week='date +%V'
-alias bell='tput bel'
 alias ccat='egrep -v "^[[:blank:]]*#|^[[:blank:]]*$"'
-alias path='echo -e "${PATH//:/\\n}"'
 alias sshni='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-
-alias reload="exec ${SHELL} -l"
