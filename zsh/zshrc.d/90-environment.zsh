67d814383c6efc1a24d443ab17ca5dd6a15ba562
Pascal Mathis <mail@pascalmathis.com>
2020-03-29 11:42:38 +0200

    zsh: Add Cargo binaries to PATH

--- zsh/zshrc.d/90-environment.zsh
+++ zsh/zshrc.d/90-environment.zsh
@@ -1,4 +1,4 @@
-export PATH="${DOT}/bin:${HOME}/go/bin:${HOME}/.npm/bin:${PATH}"
+export PATH="${DOT}/bin:${HOME}/.cargo/bin:${HOME}/go/bin:${HOME}/.npm/bin:${PATH}"
 export EDITOR="$(command -v vim)"
 export PYTHONIOENCODING="UTF-8"
 export MANPAGER="less -X"
