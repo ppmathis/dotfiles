ebb94e65facef07376cfcaf802804dc3dd7a0e67
Pascal Mathis <mail@pascalmathis.com>
2020-07-12 14:06:41 +0200

    zsh: Enable Rust support for prompt

--- zsh/zshrc.d/80-spaceship-prompt.zsh
+++ zsh/zshrc.d/80-spaceship-prompt.zsh
@@ -5,6 +5,7 @@ SPACESHIP_PROMPT_ORDER=(
 	git			# Git section (git_branch + git_status)
 	node		# Node.js section
 	golang		# Go section
+	rust		# Rust section
 	docker		# Docker section
 	venv		# virtualenv section
 	exec_time	# Execution time
