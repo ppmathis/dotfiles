87bf6e9fce185984c0765bc914e0a64ddf937356
Pascal Mathis <dev@ppmathis.com>
2026-03-01 21:38:26 +0100

    feat: update shell profile

--- dot_config/exact_shell/profile.sh
+++ dot_config/exact_shell/profile.sh
@@ -4,7 +4,7 @@
 # Paths
 dot_path_append "${HOME}/.local/bin"
 dot_path_append "${HOME}/go/bin"
-dot_path_append "${HOME}/.composer/vendor/bin"
+dot_path_append "${HOME}/.config/composer/vendor/bin"
 dot_path_append "${HOME}/Library/Application Support/JetBrains/Toolbox/scripts"
 
 # Configs
@@ -39,14 +39,4 @@ fi
 # Local Profile
 if [ -f "${HOME}/.local.sh" ]; then
   . "${HOME}/.local.sh"
-fi
-
-# Herd
-if [ -d "/Applications/Herd.app" ]; then
-  dot_path_append "${HOME}/Library/Application Support/Herd/bin/"
-  for _version_dir in "$HOME/Library/Application Support/Herd/config/php/"*/; do
-    _version_name="$(basename "${_version_dir%/}")"
-    export "HERD_PHP_${_version_name}_INI_SCAN_DIR=${_version_dir}"
-  done
-  unset _version_dir _version_name
 fi
