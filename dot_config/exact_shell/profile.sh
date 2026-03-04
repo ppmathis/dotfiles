5e909df063dd4ce4cc2e2a6d3846311fc6bad2e9
Pascal Mathis <dev@ppmathis.com>
2026-03-04 21:39:12 +0100

    feat: support pnpm in shell profile

--- dot_config/exact_shell/profile.sh
+++ dot_config/exact_shell/profile.sh
@@ -6,6 +6,7 @@ dot_path_append "${HOME}/.local/bin"
 dot_path_append "${HOME}/go/bin"
 dot_path_append "${HOME}/.config/composer/vendor/bin"
 dot_path_append "${HOME}/Library/Application Support/JetBrains/Toolbox/scripts"
+dot_path_append "${HOME}/Library/pnpm"
 
 # Configs
 export UV_NATIVE_TLS="true"
@@ -24,6 +25,11 @@ if [ -d "/opt/homebrew/opt/nvm" ]; then
   export NVM_DIR="${HOME}/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+fi
+
+# PNPM
+if [ -d "${HOME}/Library/pnpm" ]; then
+  export PNPM_HOME="${HOME}/Library/pnpm"
 fi
 
 # Rustup
